7485f5d36dd52431b2ef43f08591eddbceff1fdb
Vladyslav Teteruk <teteryk.vladislav@gmail.com>
2023-09-12 17:44:51 +0300

    Some changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,25 @@
-// Add imports above this line
+
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+import "simplelightbox/dist/simple-lightbox.min.css";
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
-// Change code below this line
-
+function createGalleryItemsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`
+  }).join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const playerContainer = document.getElementById('vimeo-player-container');
+const videoUrl = 'https://player.vimeo.com/video/236203659';
+
+
+const player = new Player(playerContainer, {
+  url: videoUrl,
+  autoplay: true, 
+});
+
+
+const saveCurrentTime = throttle(async () => {
+  try {
+    const currentTime = await player.getCurrentTime();
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  } catch (error) {
+    console.error('Error saving current time:', error);
+  }
+}, 1000); 
+
+
+player.on('timeupdate', saveCurrentTime);
+
+
+window.addEventListener('load', async () => {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    try {
+      await player.setCurrentTime(parseFloat(currentTime));
+    } catch (error) {
+      console.error('Error setting current time:', error);
+    }
+  }
+});
