2114a61d3ccc1f61e970fe48f99205b2a4f82795
Vladyslav Teteruk <teteryk.vladislav@gmail.com>
2023-09-12 18:03:06 +0300

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+
+
+const updateLocalStorage = throttle(() => {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500);
+
+
+form.addEventListener('input', updateLocalStorage);
+
+
+function restoreFormData() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email;
+    messageTextarea.value = formData.message;
+  }
+}
+
+window.addEventListener('load', restoreFormData);
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageTextarea.value = '';
+});
+// jst check
