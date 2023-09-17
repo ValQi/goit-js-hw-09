92ee814fc79019e4cb9150cad61b6df41cd0a2d9
Vladyslav Teteruk <teteryk.vladislav@gmail.com>
2023-09-17 14:58:52 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -19,12 +19,3 @@ const getCurrentTime = function (currentTime) {
 player.on('timeupdate', throttle(getCurrentTime, 1000));
 
 player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
-
-player
-  .setColor('#d8e0ff')
-  .then(function (color) {
-    console.log('The new color value: #D8E0FF');
-  })
-  .catch(function (error) {
-    console.log('An error occurred while setting the color');
-  });
