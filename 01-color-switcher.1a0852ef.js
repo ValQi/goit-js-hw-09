4ec5b679d8075d172817dad89f3ca9628fe8be11
ValQi <131767148+ValQi@users.noreply.github.com>
2023-09-23 13:49:08 +0000

    Deploying to gh-pages from @ ValQi/goit-js-hw-09@1c5b9c5345778c3d93a466768ccbfcd830bc7cac 🚀

---
+++ 01-color-switcher.1a0852ef.js
@@ -0,0 +1,3 @@
+let e=null;const t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");t.addEventListener("click",()=>{t.disabled=!0,d.disabled=!1,e=setInterval(()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`},1e3)}),d.addEventListener("click",()=>{t.disabled=!1,d.disabled=!0,clearInterval(e)});//# sourceMappingURL=01-color-switcher.1a0852ef.js.map
+
+//# sourceMappingURL=01-color-switcher.1a0852ef.js.map
